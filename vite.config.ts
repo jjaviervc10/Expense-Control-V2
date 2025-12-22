a2f99645e723a4fcbe0c3f8e3699e40590908a36
unknown <v.cjavier.velazquez@gmail.com>
2025-12-21 22:34:35 -0600

    Creacion de Icono PNG que nos servira para nuestra app sea instalable tipo PWA, configuracion de vite.config.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'opcionA.png'], // si tienes más, los agregas aquí
+      manifest: {
+        name: 'Control de Gastos',
+        short_name: 'Gastos',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#4F83F8',
+        icons: [
+          {
+            src: '/opcionA.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 })
